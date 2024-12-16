27f8078e182a2d79a4d6e3ad72d8999db42c5c01
Hussain <g202316090@kfupm.edu.sa>
2024-12-16 22:30:19 +0300

    Fix error in getByEmail for AuthorRepo

--- src/main/java/com/example/skillhub/domain/Author.java
+++ src/main/java/com/example/skillhub/domain/Author.java
@@ -20,6 +20,7 @@ public class Author {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    @Column(unique = true)
     private String email;
     private String phone;
     private String password;
--- src/main/java/com/example/skillhub/repositories/AuthorRepository.java
+++ src/main/java/com/example/skillhub/repositories/AuthorRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 @Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 
-    Optional<Object> findByEmail(String email);
+    Optional<Author> findByEmail(String email);
 }
